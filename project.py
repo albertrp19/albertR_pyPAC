e5977b3db8de8f3ca759428a422a7ef0b06266c0
albertrp19 <albertrp1909@gmail.com>
2024-04-24 13:07:31 +0200

    menu navegació

--- project.py
+++ project.py
@@ -1,1 +1,24 @@
-print("Hola")
+import os
+
+def menu():
+    print("╔═══════════════════════════════════════════════════════════════╗")
+    print("║  BENVINGUTS AL LLIBRE DE LES ACCEPCIONS DE L'ALBERT RODIGUEZ  ║")
+    print("╠═══════════════════════════════════════════════════════════════╣")
+    print("║                                                               ║")
+    print("║  1).   Afegir paraula i accepció                              ║")
+    print("║  2).   Mostrar paraula i accepcions                           ║")
+    print("║  3).   Eliminar paraula i accepció                            ║")
+    print("║  4).   Sortir                                                 ║")
+    print("║                                                               ║")
+    print("╚═══════════════════════════════════════════════════════════════╝")
+
+opcio = 0
+
+while opcio != 4:
+    menu()
+    opcio = int(input("Introdueix una opcio valida: "))
+    while opcio > 4 or opcio < 1:
+        os.system('cls')
+        print("Ha introduit un opcio no valida")
+        menu()
+        opcio = int(input("Introdueix una opcio valida: "))
