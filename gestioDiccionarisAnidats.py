6c211a5f9d5d0fa4dbbb771c20d29d86568a0325
albertrp19 <albertrp1909@gmail.com>
2024-04-24 12:27:56 +0200

    Gestio Diccionari

---
+++ gestioDiccionarisAnidats.py
@@ -0,0 +1,25 @@
+diccionari = {}
+
+print(diccionari)
+
+paraula = "xarxa"
+entrada = {
+  "PESCA" : "Ormeig de pescar constituït per un teixit de fils nuats formant una retícula quadrada o rombal."}
+
+diccionari.update({paraula:entrada})
+
+print(diccionari)
+
+auxiliar = diccionari[paraula]
+
+print(auxiliar)
+
+entrada = {
+  "TÈXTIL" : "Teixit de les xarxes de pescar, fabricat amb torçal de cotó o amb fil d’abacà."}
+  
+auxiliar.update(entrada)
+
+
+diccionari.update({paraula:auxiliar})
+
+print(diccionari)
