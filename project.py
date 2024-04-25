0b86ab6d261f4f9cf1dc5397252ae3e52bbf81ba
albertrp19 <albertrp1909@gmail.com>
2024-04-25 11:58:58 +0200

    funcions

--- project.py
+++ project.py
@@ -1,37 +1,5 @@
 import os
-
-def menu():
-    print("╔═══════════════════════════════════════════════════════════════╗")
-    print("║  BENVINGUTS AL LLIBRE DE LES ACCEPCIONS DE L'ALBERT RODIGUEZ  ║")
-    print("╠═══════════════════════════════════════════════════════════════╣")
-    print("║                                                               ║")
-    print("║  1).   Afegir paraula i accepció                              ║")
-    print("║  2).   Mostrar paraula i accepcions                           ║")
-    print("║  3).   Eliminar paraula i accepció                            ║")
-    print("║  4).   Sortir                                                 ║")
-    print("║                                                               ║")
-    print("╚═══════════════════════════════════════════════════════════════╝")
-
-def menu1():
-    print("╔═══════════════════════════════════════════════════════════════╗")
-    print("║                   Afegir paraula i accepció                   ║")
-    print("╚═══════════════════════════════════════════════════════════════╝")
-        
-def menu2():
-    print("╔═══════════════════════════════════════════════════════════════╗")
-    print("║                 Mostrar paraula i accepcions                  ║")
-    print("╚═══════════════════════════════════════════════════════════════╝")
-    
-def menu3():
-    print("╔═══════════════════════════════════════════════════════════════╗")
-    print("║                  Eliminar paraula i accepció                  ║")
-    print("╚═══════════════════════════════════════════════════════════════╝")
-
-def menu4():
-    print("╔═══════════════════════════════════════════════════════════════╗")
-    print("║                            Sortir                             ║")
-    print("╚═══════════════════════════════════════════════════════════════╝")
-    print("Adeu, que tinguis un bon dia")
+from funcions import *
 opcio = 0
 
 while opcio != 4:
